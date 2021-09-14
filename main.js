35949bc0067dbf2543c2e711e89ee7992efc6e48
SimonAnastasov <simonanastasov2001@gmail.com>
2021-09-14 09:53:29 +0200

    Initial boilerplate for the friendly fire game

---
+++ main.js
@@ -0,0 +1,8 @@
+// ? Utility Function For Reference
+// function addElement(parentId, elementTag, elementId, html) {
+//     const parent = document.getElementById(parentId)
+//     const newElement = document.createElement(elementTag)
+//     newElement.setAttribute('id', elementId)
+//     newElement.innerHTML = html
+//     parent.appendChild(newElement)
+// }
